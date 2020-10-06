725f4a12df968ddb4c30cc41b40145ec75b130e3
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2020-10-06 11:12:18 -0500

    Added complement method for PlayerColor

--- src/Morris_FX/Logic/PlayerColor.java
+++ src/Morris_FX/Logic/PlayerColor.java
@@ -2,5 +2,11 @@ package Morris_FX.Logic;
 
 public enum PlayerColor {
     BLACK,
-    WHITE
+    WHITE;
+
+    public PlayerColor complement() {
+        return this == PlayerColor.BLACK ?
+                        PlayerColor.WHITE :
+                        PlayerColor.BLACK;
+    }
 }
