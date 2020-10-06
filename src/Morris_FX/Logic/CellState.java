f48f0e50bdf29a7eb286af2683a3cef17cf55f0f
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2020-10-06 11:13:01 -0500

    Added complement method for CellState

--- src/Morris_FX/Logic/CellState.java
+++ src/Morris_FX/Logic/CellState.java
@@ -4,5 +4,24 @@ public enum CellState {
     EMPTY,
     VOID,
     BLACK,
-    WHITE
-};
+    WHITE;
+
+    public CellState complement() {
+        CellState target;
+        switch (this) {
+            case BLACK:
+                target = CellState.WHITE;
+                break;
+            case WHITE:
+                target = CellState.BLACK;
+                break;
+            case EMPTY:
+                target = CellState.VOID;
+                break;
+            default:
+                target = CellState.EMPTY;
+                break;
+        }
+        return target;
+    }
+}
