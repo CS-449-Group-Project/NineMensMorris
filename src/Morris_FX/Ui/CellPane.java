eb65a70d573e795a92357c811e5b4d5dbe749a39
atparris <atparris93@gmail.com>
2020-10-08 12:20:30 -0500

    changed cellpane to have piece images

--- src/Morris_FX/Ui/CellPane.java
+++ src/Morris_FX/Ui/CellPane.java
@@ -2,11 +2,19 @@ package Morris_FX.Ui;
 
 import Morris_FX.Logic.CellPosition;
 import Morris_FX.Logic.CellState;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 
 public class CellPane extends Pane {
     private final CellPosition position;
     private final BoardPane parent;
+
+    //private Image blackPieceImage = new Image("https://www.flaticon.com/free-icon/black-circle_14", 50, 50, false, true);
+    //private BackgroundImage blackPiece = new BackgroundImage(blackPieceImage, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+
+    //private Image whitePieceImage = new Image("https://www.iconspng.com/image/75653/white-circle", 50, 50, false, true);
+    //private BackgroundImage whitePiece = new BackgroundImage(whitePieceImage, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+
 
     public CellPane(CellPosition position, BoardPane boardPane) {
         super();
@@ -14,7 +22,6 @@ public class CellPane extends Pane {
         this.position = position;
         this.setPrefSize(2000, 2000);
         this.setOnMouseClicked(e -> parent.onCellClick(this));
-
         this.setState(CellState.VOID);
     }
 
@@ -25,13 +32,13 @@ public class CellPane extends Pane {
     public void setState(CellState state) {
         switch(state) {
             case BLACK:
-                setStyle("-fx-background-color: black");
+                setStyle("-fx-background-color: black; -fx-background-radius: 100");
                 break;
             case WHITE:
-                setStyle("-fx-background-color: white");
+                setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-width: 4; -fx-background-radius: 100; -fx-border-radius: 100;");
                 break;
             case VOID:
-                setStyle("-fx-background-color: grey");
+                setStyle("-fx-background-color: transparent");
                 break;
             case EMPTY:
                 setStyle(null);
