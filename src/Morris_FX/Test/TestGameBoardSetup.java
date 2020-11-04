8f83c46750a27ceec39538e14677ccecc7b0de43
atparris <atparris93@gmail.com>
2020-11-04 17:28:19 -0600

    working test cases for empty cells

--- src/Morris_FX/Test/TestGameBoardSetup.java
+++ src/Morris_FX/Test/TestGameBoardSetup.java
@@ -1,21 +1,22 @@
 package Morris_FX.Test;
 
-import Morris_FX.Logic.Board;
-import Morris_FX.Logic.GameState;
-import Morris_FX.Logic.PlayerColor;
-import Morris_FX.Logic.Turn;
+import Morris_FX.Logic.*;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import static org.junit.Assert.*;
+
 //jUnit version 5.4+
 
 public class TestGameBoardSetup {
 
+    Board board;
     // Class_GivenScenario_Expectation
     @BeforeEach
     public void setup() {
         Turn turn = new Turn(PlayerColor.BLACK);
         GameState newGame = new GameState(turn);
-        Board board = new Board(newGame);
+        board = new Board(newGame);
+        board.reset();
     }
 
     @Test
@@ -23,13 +24,21 @@ public class TestGameBoardSetup {
         // test 0,0
         // test 1,1
         // test 2,2
+        setup();
 
-        int testArray[] = {1, 2, 3};
+        CellPosition position0 = new CellPosition(0, 0);
+        Cell coordinate00 = board.getCell(position0);
 
-        for (int i : testArray)
-        {
+        CellPosition position1 = new CellPosition(1, 1);
+        Cell coordinate11 = board.getCell(position0);
 
-        }
+        CellPosition position2 = new CellPosition(2, 2);
+        Cell coordinate22 = board.getCell(position0);
+
+        assertEquals(CellState.EMPTY, coordinate00.getState());
+        assertEquals(CellState.EMPTY, coordinate11.getState());
+        assertEquals(CellState.EMPTY, coordinate22.getState());
+
     }
 
     @Test
