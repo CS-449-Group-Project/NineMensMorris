cd0ba57a65af3ba69f75e63bc8910aafd6d3395a
EXC3ll3NTrhyTHM <shadowcloud131@gmail.com>
2020-11-10 16:43:45 -0600

    better wording

--- src/Morris_FX/Logic/GameManager.java
+++ src/Morris_FX/Logic/GameManager.java
@@ -4,7 +4,7 @@ public class GameManager {
     // this class needs:
     // switchTurn()
     // all methods in GameState class
-    // game stages enumerator for piece placement stage, etc..
+    // game stages enumerated list for piece placement stage, etc..
     // getGameStage method
 
 }
