3c2e7a0f3d87ccb568541d33e780947cbfa9a76a
EXC3ll3NTrhyTHM <shadowcloud131@gmail.com>
2020-11-10 16:42:18 -0600

    Updated list of stuff to be added in GameManager

--- src/Morris_FX/Logic/GameManager.java
+++ src/Morris_FX/Logic/GameManager.java
@@ -4,5 +4,7 @@ public class GameManager {
     // this class needs:
     // switchTurn()
     // all methods in GameState class
+    // game stages enumerator for piece placement stage, etc..
+    // getGameStage method
 
 }
