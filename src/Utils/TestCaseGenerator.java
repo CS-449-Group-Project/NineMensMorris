ddd7a3bda5ab117e3424257cf4ec9acc8c123525
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2020-11-18 13:26:23 -0600

    Added gridSize validation in setup method

--- src/Utils/TestCaseGenerator.java
+++ src/Utils/TestCaseGenerator.java
@@ -1,6 +1,7 @@
 package Utils;
 
 import Morris_FX.Logic.CellPosition;
+import com.sun.media.sound.InvalidFormatException;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -17,23 +18,32 @@ public class TestCaseGenerator implements  Iterable<CellPosition> {
     private Vector<CellPosition> moves = new Vector<>(10);
     private Map<String, CellPosition> movesCache = new HashMap<>(10);
     private String filePath;
+    private int gridSize;
     public TestCaseGenerator(String filePath) throws IOException {
         this.filePath = filePath;
         setup();
     }
 
-    private void setup() throws IOException {
+    public int getExpectedGridSize() {
+        return gridSize;
+    }
+
+    private void setup() throws IOException, NumberFormatException {
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        String line;
+        String line = reader.readLine().trim();
+        gridSize = Integer.parseInt(line);
+        if (gridSize > 99) {
+            throw new InvalidFormatException("gridSize must be less than 100.");
+        }
         while ((line = reader.readLine()) != null) {
-            if (line.trim().length() == 0) {
+            String trimmedLine = line.trim();
+            if (trimmedLine.length() == 0) {
                 continue;
             }
             String humanName = line.substring(0, 2);
             if (!movesCache.containsKey(humanName)) {
-                movesCache.put(humanName, CellPosition.createFromHumanString(humanName));
+                movesCache.put(humanName, CellPosition.createFromHumanString(gridSize, humanName));
             }
-
             CellPosition cellPos = movesCache.get(humanName);
             moves.add(cellPos);
         }
