29773f607c03c43b5339c54dd13c2ce48b749671
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2020-11-19 19:32:17 -0600

    Simplified TestCaseGenerator

--- src/Utils/TestCaseGenerator.java
+++ src/Utils/TestCaseGenerator.java
@@ -1,54 +1,75 @@
 package Utils;
 
 import Morris_FX.Logic.CellPosition;
-import com.sun.media.sound.InvalidFormatException;
+import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Vector;
-import java.util.function.Consumer;
 
 public class TestCaseGenerator implements  Iterable<CellPosition> {
 
     private Vector<CellPosition> moves = new Vector<>(10);
-    private Map<String, CellPosition> movesCache = new HashMap<>(10);
-    private String filePath;
+    private Map<Short, CellPosition> movesCache = new HashMap<>(10);
     private int gridSize;
-    public TestCaseGenerator(String filePath) throws IOException {
-        this.filePath = filePath;
-        setup();
+
+
+    public static TestCaseGenerator createFromFile(String filePath) throws IOException {
+        TestCaseGenerator testCaseGenerator = new TestCaseGenerator();
+        BufferedInputStream reader = new BufferedInputStream(new FileInputStream(filePath));
+        int headerLength = 8;
+        byte[] header = new byte[headerLength];
+        reader.read(header, 0, headerLength);
+
+        int magicNumber = readTwoBytesAsShort(header,0);
+        if (magicNumber != TestFileDataGenerator.magicNumber) {
+            throw new Error("Invalid magic number");
+        }
+        // int version = header[1] << ; // ignore version number for now
+
+        int gridSizeOffset = 2 * 2;
+        int gridSize = readTwoBytesAsShort(header, gridSizeOffset);
+        testCaseGenerator.setGridSize(gridSize);
+
+        int positionCountOffset = 3 * 2;
+        short positionsAmount = readTwoBytesAsShort(header, positionCountOffset);
+        byte[] compressedPositions = new byte[positionsAmount * 2];
+        reader.read(compressedPositions);
+        testCaseGenerator.setupPositionsFromByteArray(compressedPositions);
+        return testCaseGenerator;
+    }
+
+    private void setupPositionsFromByteArray(byte[] compressedPositions) {
+        for (int i = 0; i < compressedPositions.length; i += 2) {
+            short compressedPosition = readTwoBytesAsShort(compressedPositions, i);
+            if (!movesCache.containsKey(compressedPosition)) {
+                movesCache.put(compressedPosition, decompressPosition(compressedPosition));
+            }
+            CellPosition cellPos = movesCache.get(compressedPosition);
+            moves.add(cellPos);
+        }
+    }
+
+    private static short readTwoBytesAsShort(byte[] buffer, int offset) {
+        short value = (short)(buffer[offset] << 8 | buffer[offset + 1]);
+        return value;
     }
 
     public int getExpectedGridSize() {
         return gridSize;
     }
 
-    private void setup() throws IOException, NumberFormatException {
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        String line = reader.readLine().trim();
-        gridSize = Integer.parseInt(line);
-        if (gridSize > 99) {
-            throw new InvalidFormatException("gridSize must be less than 100.");
-        }
-        while ((line = reader.readLine()) != null) {
-            String trimmedLine = line.trim();
-            if (trimmedLine.length() == 0) {
-                continue;
-            }
-            String humanName = line.substring(0, 2);
-            if (!movesCache.containsKey(humanName)) {
-                movesCache.put(humanName, CellPosition.createFromHumanString(gridSize, humanName));
-            }
-            CellPosition cellPos = movesCache.get(humanName);
-            moves.add(cellPos);
-        }
+    private void setGridSize(int gridSize) {
+        this.gridSize = gridSize;
     }
 
+    private CellPosition decompressPosition(short compressedPosition) {
+        int column = compressedPosition >> 8;
+        int row = compressedPosition & 255;
+        return new CellPosition(column, row);
+    }
 
     @Override
     public Iterator<CellPosition> iterator() {
