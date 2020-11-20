e8c8d9df64a2b8bf2e0821c42316a12be4e17f13
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2020-11-19 19:32:05 -0600

    Simplified TestFileDataGenerator

--- src/Utils/TestFileDataGenerator.java
+++ src/Utils/TestFileDataGenerator.java
@@ -2,43 +2,27 @@ package Utils;
 
 import Morris_FX.Logic.CellPosition;
 
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Vector;
 
 public class TestFileDataGenerator {
+    public static short magicNumber = 0x5444; // TD
     private int gridSize = 0;
-    private Vector<CellPosition> positions;
-    private Vector<String> comments;
+    private short version = 0;
+    private Vector<CellPosition> positions = new Vector<>(10);
     public TestFileDataGenerator(int gridSize) {
         this.gridSize = gridSize;
-        positions = new Vector<>(10);
-        comments = new Vector<>(10);
     }
 
     public void addPosition(CellPosition position) {
         positions.add(position);
     }
 
-    public void addComment(String comment) {
-        comments.add(comment);
-    }
-
-    public void updateLastComment(String extraComment) {
-        String lastComment =  comments.remove(comments.size() - 1);
-        comments.add(lastComment + extraComment);
-    }
-
     public boolean generateFile(String filePath) {
         try {
-            FileWriter fileWriter = new FileWriter(filePath);
-            fileWriter.append(gridSize + "\n");
-            for (int i = 0; i < positions.size(); i++) {
-                CellPosition pos = positions.get(i);
-                String varName = pos.getHumanString();
-                String lineComment = comments.get(i);
-                fileWriter.append(String.format("%s %s\n", varName, lineComment));
-            }
+            FileOutputStream fileWriter = new FileOutputStream(filePath);
+            fileWriter.write(convertDataToByteArray());
             fileWriter.close();
         } catch (IOException ioException) {
             ioException.printStackTrace();
@@ -46,8 +30,38 @@ public class TestFileDataGenerator {
         }
         return true;
     }
+
+    private byte[] convertDataToByteArray() {
+        int headerSize = 4;
+        int size = positions.size() + headerSize;
+        byte[] stream = new byte[size * 2];
+        int offset = writeShortToBuffer(stream, 0, magicNumber);
+
+        offset = writeShortToBuffer(stream, offset, version);
+        offset = writeShortToBuffer(stream, offset, (short)gridSize);
+        offset = writeShortToBuffer(stream, offset, (short)positions.size());
+
+        for (int i = 0; i < positions.size(); i++) {
+            CellPosition pos = positions.get(i);
+            short compressedPos = compressPosition(pos);
+            offset = writeShortToBuffer(stream, offset, compressedPos);
+        }
+        return stream;
+    }
+
+    private int writeShortToBuffer(byte[] buffer, int offset, short value) {
+        buffer[offset] = (byte)(value >> 8);
+        buffer[offset + 1] = (byte)(value & 0xFF);
+        return offset + 2;
+    }
+
+
+    private short compressPosition(CellPosition pos) {
+        // column | row
+        short compressedPos = (short)((pos.getColumn() << 8) | pos.getRow());
+        return compressedPos;
+    }
     public void reset() {
         positions.clear();
-        comments.clear();
     }
 }
