a8ef5c0b91e2b3906f9157d2b5f8987a9aed63ed
atparris <atparris93@gmail.com>
2020-12-06 13:05:54 -0600

    fixed test_files branch

--- src/Morris_FX/Test/TestFlyRule.java
+++ src/Morris_FX/Test/TestFlyRule.java
@@ -3,72 +3,37 @@ package Morris_FX.Test;
 import Morris_FX.Logic.*;
 import Morris_FX.Ui.BoardPane;
 import Morris_FX.Ui.CellPane;
+import Utils.TestCaseGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestFlyRule {
     private Board board;
     private GameManager gameManager;
     private BoardPane boardPane;
-    private Player player;
 
     @BeforeEach
     private void setup() {
         gameManager = new GameManager();
         board = new Board(gameManager);
-        player = gameManager.getCurrentPlayer();
         boardPane = new BoardPane(board, gameManager);
         board.reset();
-        player.reset();
-
-        // piece movement happens when no pieces in hand
-        while (gameManager.getActivePlayer().hasPiecesInHand()) {
-            gameManager.getActivePlayer().removePiecesFromHand();
-            gameManager.getInactivePlayer().removePiecesFromHand();
-        }
     }
 
     @Test
-    public void GameManager_GivenPlayerReachesThreePieces_FlyRuleImplemented()
+    public void GameManager_GivenPlayerReachesThreePieces_FlyRuleImplemented() throws IOException
     {
-        gameManager.getActivePlayer().setGamePhase(Player.Phase.PIECE_MOVEMENT);
-        gameManager.getInactivePlayer().setGamePhase(Player.Phase.PIECE_MOVEMENT);
-        // black cells
-        CellPane coordinate00 = board.getCell(new CellPosition(0, 0));
-        CellPane coordinate30 = board.getCell(new CellPosition(3, 0));
-        CellPane coordinate03 = board.getCell(new CellPosition(0, 3));
-        CellPane coordinate36 = board.getCell(new CellPosition(3, 6));
+        TestCaseGenerator testCaseObject = TestCaseGenerator.createFromFile("./test-inputs/flyRuleForWhite.td");
 
-        // white cells
-        CellPane coordinate11 = board.getCell(new CellPosition(1, 1));
-        CellPane coordinate22 = board.getCell(new CellPosition(2, 2));
-        CellPane coordinate44 = board.getCell(new CellPosition(4, 4));
-        CellPane coordinate55 = board.getCell(new CellPosition(5, 5));
+        assertEquals(testCaseObject.getExpectedGridSize(), Board.GRID_SIZE);
 
-        // black moves here to form mill
-        CellPane coordinate06 = board.getCell(new CellPosition(0, 6));
-
-        coordinate00.setState(CellState.BLACK);
-        coordinate30.setState(CellState.BLACK);
-        coordinate03.setState(CellState.BLACK);
-        coordinate36.setState(CellState.BLACK);
-
-        coordinate11.setState(CellState.WHITE);
-        coordinate22.setState(CellState.WHITE);
-        coordinate44.setState(CellState.WHITE);
-        coordinate55.setState(CellState.WHITE);
-
-        // hard code board piece population
-
-        while(gameManager.getActivePlayer().getBoardPieces() < 4)
-        {
-            gameManager.getActivePlayer().increaseBoardPieces();
-            gameManager.getInactivePlayer().increaseBoardPieces();
+        for (CellPosition recordedPos: testCaseObject) {
+            gameManager.performMove(board.getCell(recordedPos));
         }
-        gameManager.performMove(coordinate36); // piece to move
-        gameManager.performMove(coordinate06); // black forms mill
-        gameManager.performMove(coordinate11); // black removes this white piece
 
         assertEquals(PlayerColor.WHITE, gameManager.getActivePlayer().getColor());
         assertEquals(Player.Phase.FLY_RULE, gameManager.getActivePlayer().getGamePhase());
