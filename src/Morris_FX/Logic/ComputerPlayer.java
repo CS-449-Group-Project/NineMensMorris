20b30b66c7cd21545f880b123319913b62f9e6a0
Blake Simpson <shadowcloud131@gmail.com>
2020-12-09 12:41:04 -0600

    Updated how computer player perfoms move during their turn

--- src/Morris_FX/Logic/ComputerPlayer.java
+++ src/Morris_FX/Logic/ComputerPlayer.java
@@ -5,18 +5,40 @@ import Morris_FX.Ui.CellPane;
 public class ComputerPlayer extends Player {
     private Board board;
     private GameManager gameManager;
+    private int x, y;
 
     public void onComputerTurn() {
-        CellPane cell = getCellForComputerTurn();
 
+        // always performMove at least once
+        CellPane cell = getCellForComputerTurn();
+        gameManager.performMove(cell);
+
+        // performMove again if not Piece Placement phase
+        if (getGamePhase() != Phase.PIECE_PLACEMENT) {
+            cell = getCellForComputerTurn();
+            gameManager.performMove(cell);
+        }
     }
 
     private CellPane getCellForComputerTurn() {
         // This position should have some logic to determine it
-        CellPosition pos = new CellPosition(0,0);
-        //
+        CellPane cellPane;
+        do {
+            // generate random x and y
+            x = (int) (Math.random() * board.GRID_SIZE);
+            y = (int) (Math.random() * board.GRID_SIZE);
+            cellPane = new CellPane(new CellPosition(x, y));
+        } while (!board.validateCellSelection(cellPane));
+        return cellPane;
+    }
 
-        return new CellPane(pos);
+    public int[] getCoordinates() {
+        int[] coordinates = new int[2];
+
+        coordinates[0] = x;
+        coordinates[1] = y;
+
+        return coordinates;
     }
 
     public ComputerPlayer(PlayerColor color, Board board, GameManager gameManager) {
